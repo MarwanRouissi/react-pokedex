9ea0abdf0cb59247ca78467e5cf53eeda2c50edc
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-07-08 15:28:04 +0200

    changed background color + got rid of unused component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-// import logo from './logo.svg';
 import './App.css';
 import Pokedex from './Pokedex';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header> */}
       <Pokedex />
     </div>
   );
