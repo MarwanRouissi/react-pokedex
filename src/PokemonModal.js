30a5470d4cb5c4a5d5e665788c8a9165267579d5
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-07-08 13:06:19 +0200

    got rid of abilities info

--- src/PokemonModal.js
+++ src/PokemonModal.js
@@ -16,14 +16,16 @@ function PokemonModal({ pokemon, onClose }) {
       }
 
       // Extraire les noms et les valeurs des statistiques
-      const statNames = pokemon.stats.map(statInfo => statInfo.stat.name);
+      // const statNames = pokemon.stats.map(statInfo => statInfo.stat.name);
       const statValues = pokemon.stats.map(statInfo => statInfo.base_stat);
 
       // Créer un nouveau graphique
       chartInstance.current = new Chart(ctx, {
         type: 'radar',
         data: {
-          labels: statNames,
+          labels: [
+            "HP", "ATK", "DEF", "SATK", "SDEF", "SPD"
+          ],
           datasets: [{
             label: 'Stats',
             data: statValues,
@@ -34,12 +36,14 @@ function PokemonModal({ pokemon, onClose }) {
           }],
         },
         options: {
+          responsive: true,
           plugins: {
             legend: {
               display: false,
             },
           },
         },
+
       });
     }
   }, [pokemon]);
@@ -51,41 +55,50 @@ function PokemonModal({ pokemon, onClose }) {
       <div className="modal-content" onClick={e => e.stopPropagation()}>
         <button className="close-button" onClick={onClose}>X</button>
         <div className="header">
-          <h2>ID: {pokemon.id}</h2>
-          <h2>{pokemon.name}</h2>
+          <h3>ID: {pokemon.id}</h3>
         </div>
         <img id="modal-img" src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name} />
+        <h2>{pokemon.name}</h2>
+        <div className="pokemon-types">
+          {pokemon.types.map((typeInfo, index) => (
+            <span key={index} className="pokemon-type">{typeInfo.type.name}</span>
+          ))}
+        </div>
         <h3>About</h3>
         <div className="about">
           <div className="height">
             <div className="height-details">
-              <img width="48" height="48" src="https://img.icons8.com/badges/48/ruler-vertical.png" alt="ruler-vertical"/>
+              <img width="40" height="40" src="https://img.icons8.com/badges/48/ruler-vertical.png" alt="ruler-vertical"/>
               <p>{pokemon.height / 10} M</p>
             </div>
             <p>Height</p>
           </div>
           <div className="weight">
             <div className="weight-details">
-              <img width="50" height="50" src="https://img.icons8.com/ios/50/weight.png" alt="weight"/>
+              <img width="40" height="40" src="https://img.icons8.com/ios/50/weight.png" alt="weight"/>
               <p>{pokemon.weight / 10} KG</p>
             </div>
             <p>Weight</p>
           </div>
-          <div className="pokemon-abilities">
-            {pokemon.abilities.map((abilityInfo, index) => (
-              <span key={index} className="pokemon-ability">{abilityInfo.ability.name}</span>
-            ))}
-            <p>Abilities</p>
+          {/* <div className="abilities">
+            <div className="pokemon-abilities">
+              {pokemon.abilities.map((abilityInfo, index) => (
+                <span key={index} className="pokemon-ability">{abilityInfo.ability.name}</span>
+              ))}
+              <p>Abilities</p>
           </div>
+          </div> */}
         </div>
+        {/* <canvas ref={chartRef}></canvas> */}
+        <h3>Base Stats</h3>
         <div className="pokemon-stats">
           <canvas ref={chartRef}></canvas>
         </div>
-        <div className="pokemon-types">
+        {/* <div className="pokemon-types">
           {pokemon.types.map((typeInfo, index) => (
             <span key={index} className="pokemon-type">{typeInfo.type.name}</span>
           ))}
-        </div>
+        </div> */}
       </div>
     </div>
   );
